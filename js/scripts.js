370081246d6a650be0078b08f4b34e76a4f6e2f1
schuabjp <joaoschuab@outlook.com>
2023-05-09 20:02:33 -0300

    video selector add

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,27 @@ function myFunction() {
         x.className = x.className.replace("show", "");
     }
 }
+
+function novoVideo(video) {
+    switch (video) {
+        case '1':
+            document.getElementById('videoSelecionado').src = 'https://www.youtube.com/embed/ciR3sUdgma0';
+            document.getElementById('video1').classList.add("execucao");
+            document.getElementById('video2').classList.remove("execucao");
+            document.getElementById('video3').classList.remove("execucao");
+            break;
+        case '2':
+            document.getElementById('videoSelecionado').src = 'https://www.youtube.com/embed/GdzrrWA8e7A';
+            document.getElementById('video2').classList.add("execucao");
+            document.getElementById('video1').classList.remove("execucao");
+            document.getElementById('video3').classList.remove("execucao");
+            break;
+        case '3':
+            document.getElementById('videoSelecionado').src = 'https://www.youtube.com/embed/HxOBgWHVUzo';
+            document.getElementById('video3').classList.add("execucao");
+            document.getElementById('video1').classList.remove("execucao");
+            document.getElementById('video2').classList.remove("execucao");
+            break;
+    }
+}
+
